20a7b8f26d2d9c708aa8eba99047aeca40ef8029
Henrik Tóth <realthk@gmail.com>
2024-07-13 15:03:35 +0200

    error handling

--- custom_components/pollen_hu/sensor.py
+++ custom_components/pollen_hu/sensor.py
@@ -1,10 +1,9 @@
-import asyncio
+import aiohttp
 import json
 import logging
 import re
 import voluptuous as vol
-import aiohttp
-import random
+from datetime import datetime
 from datetime import timedelta
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
@@ -17,30 +16,53 @@ REQUIREMENTS = [ ]
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_ALLDOMINANT = 'all_dominant'
 CONF_ATTRIBUTION = "Data provided by antsz.hu"
 CONF_NAME = 'name'
+CONF_POLLENS = 'pollens'
+CONF_SSL = 'ssl'
 
-DEFAULT_NAME = 'Pollen HU'
+DEFAULT_ALLDOMINANT = False
 DEFAULT_ICON = 'mdi:blur'
+DEFAULT_NAME = 'Pollen HU'
+DEFAULT_SSL = True
 
 SCAN_INTERVAL = timedelta(hours=1)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_ALLDOMINANT, default=DEFAULT_ALLDOMINANT): cv.boolean,
+    vol.Optional(CONF_POLLENS, default=[]): vol.All(cv.ensure_list, [cv.string]),
+    vol.Optional(CONF_SSL, default=DEFAULT_SSL): cv.boolean,
 })
 
 async def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
+    alldominant = config.get(CONF_ALLDOMINANT)
+    pollens = config.get(CONF_POLLENS)
+    ssl = config.get(CONF_SSL)
 
     async_add_devices(
-        [PollenHUSensor(hass, name )],update_before_add=True)
+        [PollenHUSensor(hass, name, alldominant, pollens, ssl)],update_before_add=True)
 
 async def async_get_pdata(self):
-    pjson = {}
+    pjson = {"pollens": []}
+    pjson1 = {}
+    successful_poll = "true"
 
     url = 'https://efop180.antsz.hu/polleninformaciok/'
-    async with self._session.get(url) as response:
-        rsp1 = await response.text()
+    try:
+        async with self._session.get(url) as response:
+            rsp1 = await response.text()
+        if response is None or response.status != 200:
+            rsp1 = ""
+            successful_poll = "false"
+    except(aiohttp.client_exceptions.ClientConnectorError):
+            rsp1 = ""
+            successful_poll = "false"
+    except(asyncio.TimeoutError.TimeoutError):
+            rsp1 = ""
+            successful_poll = "false"
 
     rsp = rsp1.replace("\n","").replace("\r","")
 
@@ -57,89 +79,81 @@ async def async_get_pdata(self):
              .replace(" \"","\",\"") \
              .replace("\",\"name","\"},{\"name") \
              .replace(">", "{\"pollens\":[{") + "\"}]}"
-        pjson = json.loads(p3)
+        pjson1 = json.loads(p3)
+        if len(self._pollens) != 0:
+            i = 0
+            for item in pjson1['pollens']:
+                if item.get('name') not in self._pollens:
+                    continue
+                else:
+                    pjson['pollens'].append({"name": item.get('name'),"value": item.get('value')})
+                    i += 1;
+        else:
+            pjson = pjson1
+    pjson['successful_poll'] = successful_poll
     return pjson
 
 class PollenHUSensor(Entity):
 
-    def __init__(self, hass, name):
+    def __init__(self, hass, name, alldominant, pollens, ssl):
         """Initialize the sensor."""
         self._hass = hass
         self._name = name
+        self._alldominant = alldominant
         self._state = None
+        self._last_poll = ""
         self._pdata = []
+        self._pollens = pollens
+        self._ssl = ssl
+        self._successful_poll = "true"
         self._icon = DEFAULT_ICON
-        self._session = async_get_clientsession(hass)
+        self._session = async_get_clientsession(hass, ssl)
 
     @property
     def extra_state_attributes(self):
         attr = {}
-        dominant_value = 0
+        dominant_value = -1
 
         if 'pollens' in self._pdata:
             attr["pollens"] = self._pdata.get('pollens')
-            num_values = [0, 0, 0, 0, 0]
 
             for item in self._pdata['pollens']:
-                value = int(item.get('value'))
-                if value>1:
-                    num_values[value]+=1
-                if value>dominant_value:
-                    dominant_value=value
-
-            attr["dominant_pollen_value"] = dominant_value
-            attr["dominant_pollen"] = ""
-
-            if dominant_value>0:
-                attr["dominant_pollen"] = self.get_dominant_text(dominant_value)
-                if dominant_value>2:
-                    ext = self.get_dominant_text(dominant_value-1).lower()
-                    if ext>'':
-                        attr["dominant_pollen"] += ' ' + random.choice(['illetve','továbbá','azon kívül','ezen felül','és azt mondják,'])+' '
-                        attr["dominant_pollen"] += ext
-                if dominant_value==4 and num_values[2]>0 and num_values[2]<5:
-                    ext = self.get_dominant_text(dominant_value-2).lower()
-                    if ext>'':
-                        attr["dominant_pollen"] += ' ' + random.choice(['illetve','továbbá','azon kívül','ezen felül','és azt mondják,'])+' '
-                        attr["dominant_pollen"] += ext
-                attr["dominant_pollen"] += "."
+                val = item.get('value')
+                if int(val) > dominant_value:
+                    attr["dominant_pollen_value"] = int(val)
+                    attr["dominant_pollen"] = item.get('name')
+                    dominant_value = int(val)
+                    attr["dominant_pollens_nr"] = 1
+                elif int(val) == dominant_value and self._alldominant:
+                    if 'dominant_pollen' in attr:
+                        attr["dominant_pollen"] = attr["dominant_pollen"] + "|" + item.get('name')
+                        attr["dominant_pollens_nr"] = attr["dominant_pollens_nr"] + 1
+                    else:
+                        attr["dominant_pollen"] = item.get('name')
+                        attr["dominant_pollens_nr"] = 1
+                    attr["dominant_pollen_value"] = int(val)
+                    dominant_value = int(val)
 
         attr["provider"] = CONF_ATTRIBUTION
+        attr["last_poll"] = self._last_poll
+        attr["successful_poll"] = self._successful_poll
         return attr
-
-    def get_dominant_text(self, level):
-        ret = ""
-        koncentraciok=["", "Alacsony", "Közepes", "Magas", "Nagyon magas"]
-        dominansok = []
-        for item in self._pdata['pollens']:
-            if int(item.get('value'))==level:
-                dominansok.append(self.nevelo(item.get('name')) + " " + item.get('name').lower())
-        for i in range(len(dominansok)):
-            if i>0:
-                if i==len(dominansok)-1:
-                    ret += " és "
-                else:
-                    ret += ", "
-            ret += dominansok[i]
-        if ret>'':
-            ret=koncentraciok[level] + " koncentrációban " + ret
-
-        return ret
-
 
     async def async_update(self):
         dominant_value = 0
 
-        pdata = await async_get_pdata(self)
+        self._pdata = await async_get_pdata(self)
 
-        self._pdata = pdata
         if 'pollens' in self._pdata:
             for item in self._pdata['pollens']:
                 val = item.get('value')
                 if int(val) > dominant_value:
                     dominant_value = int(val)
 
+        dt_now = datetime.now()
+        self._last_poll = dt_now.strftime("%Y/%m/%d %H:%M")
         self._state = dominant_value
+        self._successful_poll = self._pdata['successful_poll']
         return self._state
 
     @property
@@ -153,8 +167,3 @@ class PollenHUSensor(Entity):
     @property
     def icon(self):
         return DEFAULT_ICON
-
-    def nevelo(self, szoveg):
-        if szoveg[0].upper() in ['A', 'Á', 'E', 'É', 'I', 'Í', 'O', 'Ó', 'Ö', 'Ő', 'U', 'Ú', 'Ü', 'Ű']:
-            return 'az'
-        return 'a'
